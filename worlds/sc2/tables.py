ff77efca2e7c2369cd1019f7b6f4f45827279a15
Snarky <sparkykueken@gmail.com>
2026-01-06 09:14:08 +0100

    Add comment about supporting a Nova no-builts option

--- worlds/sc2/tables.py
+++ worlds/sc2/tables.py
@@ -1,4 +1,8 @@
 class NovaPresenceOptions():
+    # Currently, if Nova is disabled in all available campaigns, Nova can still appear in no-builds
+    # In this case, all Nova items are removed from the pool and her no-builds effectively grant story tech
+    # TODO: Add an option here for Nova presence in no-builds, allowing players to exclude Nova for build missions
+    # while still requiring them to find equipment to solve the no-builds
     NCO_TERRAN = "Nova Covert Ops (Terran)"
     NCO_ZERG = "Nova Covert Ops (Zerg)"
     NCO_PROTOSS = "Nova Covert Ops (Protoss)"
