13b3b2ecc569d9deb9e381ba3cdc2840413e94dc
Snarky <sparkykueken@gmail.com>
2026-01-06 10:02:14 +0100

    Add comment for Ghost of a Chance (Auto)

--- worlds/sc2/tables.py
+++ worlds/sc2/tables.py
@@ -7,4 +7,4 @@ class NovaPresenceOptions():
     NCO_ZERG = "Nova Covert Ops (Zerg)"
     NCO_PROTOSS = "Nova Covert Ops (Protoss)"
     GHOST_OF_A_CHANCE = "Ghost of a Chance"
-    GHOST_OF_A_CHANCE_AUTO = "Ghost of a Chance (Auto)"
+    GHOST_OF_A_CHANCE_AUTO = "Ghost of a Chance (Auto)" # Use NCO Nova only if Nova is enabled in any build missions
